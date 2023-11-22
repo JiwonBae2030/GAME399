12d100f290f17855c7057dcb420418da2dfba1b0
Dahye Chung <lewachung@gmail.com>
2023-11-22 22:05:54 +0900

    Incheon : UI teleport set up

---
+++ Assets/_Dev/Dahye/Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private Transform[] positions;
+    [SerializeField] private Transform player;
+
+    public void Teleport(int num)
+    {
+        player.transform.position = positions[num].position;
+    }
+
+}
